e59bf2d740b6eb8642757ab3d55956918c119d39
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-09-25 01:08:16 +0600

    Final Push

--- js/feature.js
+++ js/feature.js
@@ -1,6 +1,16 @@
 document.getElementById('btn-show-Donation').addEventListener('click',function(){
     showSectionById('Donation-form');
-})
+    this.classList.add('active');
+    this.classList.remove('inactive');
+    document.getElementById('btn-show-transaction').classList.remove('active');
+    document.getElementById('btn-show-transaction').classList.add('inactive');
+
+});
 document.getElementById('btn-show-transaction').addEventListener('click', function () {
     showSectionById('transaction-section');
+    this.classList.add('active');
+    this.classList.remove('inactive');
+    document.getElementById('btn-show-Donation').classList.remove('active');
+    document.getElementById('btn-show-Donation').classList.add('inactive');
+
 });
